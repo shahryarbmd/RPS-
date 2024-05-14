0e9642a90e9d53ed639696f3a673f1793296abf2
shahryarbmd <53258500+shahryarbmd@users.noreply.github.com>
2024-05-14 18:12:45 +0330

    wall

--- test.py
+++ test.py
@@ -1,27 +1,184 @@
-import string
-a,b,c,d,e,f,g,h,p =  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', ' '
-boardx = [p, a,b,c,d,e,f,g,h]
-boardy = [1,2,3,4,5,6,7,8]
+import random, time
+list = ['rock', 'paper', 'scissors']
+w = "You win!"
+L = "You lost"
+T = "Its a tie!"
+play = True
+user = ""
+rw = 0
+rt = 0
+rl = 0
+sw = 0
+st = 0
+sl = 0
+pw = 0
+pt = 0
+pl = 0
+r = 0
+#we start the game with taking the "user" input
+
+class MyClass:
+    def __init__(self, user, opp):
+        self.user = user
+        self.opp = opp
+
+    def inp(self):
+        pass
+
+    def opp_inp(self):
+        opp = random.choice(list)
+        if r==0:
+           for i in range(1, 4):
+              print(i)
+              time.sleep(0.7)
+           print("opponent: ", list[opp])
+        elif (rw+rt)/r >= 0.25:
+           print("opponent: ", list[1])
+
+        elif (sw+st)/r >= 0.25:
+            print("opponent: ", list[0])
+
+        elif (pw+pt)/r >= 0.25:
+            print("opponent: ", list[2])
+        else:
+            for i in range(1, 4):
+                print(i)
+                time.sleep(0.7)
+            print("opponent: ", list[opp])
+        r += 1
+    def win(self,user,opp):
+        global rw, sw, pw
+
+        #rockw
+        if user == 0 and opp == 2:
+           rw += 1
+           time.sleep(1)
+           print(w)
+
+        #scissorw
+        if user == 2 and opp == 1:
+           sw +=1
+           time.sleep(1)
+           print(w)
+
+        #paperw
+        if user == 1 and opp == 0:
+           pw +=1
+           time.sleep(1)
+           print(w)
+        else:
+            self.tie(self,user, opp)
+
+    def tie(self,user,opp):
+        global rt,st,pt
+
+        #rockt
+        if user == 0 and opp == 0:
+           rt += 1
+           time.sleep(1)
+           print(T)
+
+        #scissort
+        if user == 2 and opp == 2:
+           st +=1
+           time.sleep(1)
+           print(T)
+
+        #papert
+        if user == 1 and opp == 1:
+           pt += 1
+           time.sleep(1)
+           print(T)
+        else:
+            self.lose(self, user, opp)
+
+    def lose(self,user,opp):
+        global sl,pl,rl
+
+        # rock LOSE
+        if user == 0 and opp == 1:
+            rl += 1
+            time.sleep(1)
+            print(L)
+
+        # scissor LOSE
+        if user == 2 and opp == 0:
+           sl += 1
+           time.sleep(1)
+           print(L)
+
+        # paper LOSE
+        if user == 1 and opp == 2:
+            pl += 1
+            time.sleep(1)
+            print(L)
+def start():
+    global play, rw, rt, rl, sw, st, sl, pw, pt, pl, r, user
+    play = True
+    while play == True:
+        x = input("rock, paper, scissors?: ")
+        time.sleep(1)
+        if x in list:
+            user = list.index(x)
+        else:
+            print("wrong input")
+            break
+    use = MyClass()
+
+    use.inp()
 
 
-print(boardx)
-print(boardy)
-for column in boardy[8::-1]:
-    print(column)
-for row in boardx[0::]:
-    print(row, end="  ")
+         #opponent does their thing
 
 
-class board:
-    def __init__(self, x, y):
-        self.num = y
-        self.let = x
-    def place(self, x, y):
-        print("enter the coordinates")
-        x = input("enter x")
-        y = input("enter y")
-        boardx.index(x) = x
-        boardy.index(y) = y
 
-#queens should have their own coordinates but how do we place them on their own
-q1= board(input("enter letter: "), input("enter number: "))
+
+
+
+
+
+         # _______WIN CONDITIONS_______
+    w = MyClass
+    w.win()
+
+
+#replayabillity_______________________________________________
+
+    if input("replay?: Y/N \n ").lower() == "y":
+
+       play = True
+
+       start()
+    else:
+
+       play = False
+       print("total games: ", r)
+       print("games won: ", rw + sw + pw)
+       print("games lost: ", rl + sl + pl)
+       exit()
+
+
+#   corny starting
+#defined a func to use in start__________________________________
+def why():
+    print("why did you come here then", end = "", flush = True)
+dot = [".",".","."]
+
+
+# this starts
+if input("wanna play?: Y/N \n").lower() == "y":
+   print("cool!")
+
+   start()
+   time.sleep(0.75)
+else:
+   why()
+   for i in dot:
+      time.sleep(0.5)
+      print(i, end="")
+   time.sleep(1)
+
+   exit()
+
+
+
--- test2.py
+++ test2.py
@@ -1,46 +1,16 @@
-import numpy as np, random
-board = np.array([[1,2,3,4,5,6,7,8],['a','b','c','d','e','f','g','h']])
-class myboard:
-    def __init__(self,x,y):
-        self.x = x
-
-        self.y = y
-
-
-    def puzzle(self,x,y ):
-        x = board[1, x]
-        y = board[0, y]
-        out = [x, y]
-        print(out)
-#!!!!!!
-# R is a random number between 0 and 8
-# q2 is the letter we receive plus a random distance
-    # NOTE
-    '''
-    while checks if q2 is inside our board to continue
-    while also checks the rules
-    '''  # NOTE OVER
-    def seek(self, q):
-        x = q.index(q[0])
-        R = random(0,9)
-        q2 = q[x + R, q[1] + R]
-
-        while q2[0] in board[1] and q2[1] in board[0] and q[0] != q2[0] and q2[1] not  :
-           #honestly idk maybe q3 goes here
-
-
-#!!!!!!
-#we need 8 queens
-
-q1 = myboard(0,1)
-y = q1.puzzle(int(input()), int(input()))
-q1.seek(y)
-
 '''
-reminder
-we need to get a queen, use it to make other queens while not breaking the rules
+basically we need to have user input as rock paper or scissors
+then we need an opponent whose choice depends on users last games experiences
+but if its the first game it has to be random
+then there are win conditions depending on both current user and opp choices
+each one win lose or tie changes a variable basically storing the info
+thus with the info opponent makes better choices
 
-turn x from seek back to its letter value
+but if the user figures the pattern out they will get winstreaks since they know
+the opponent will counter their most winning choice
+so if the user has a streak of 2 wins in a row with any choice the opponent
+has to counter the counter to the winning choice
+like
+if user_(rps)_streak >= 2:
+   opp = RPS list value
 '''
-
-
