e12af70f6ff33cbba8db2e79a1f46b1891fcdf32
shahryarbmd <53258500+shahryarbmd@users.noreply.github.com>
2024-05-22 01:14:40 +0330

    Update pycode.py

--- pycode.py
+++ pycode.py
@@ -1,47 +1,43 @@
 
-# contact book
-dic = {
-}
-
-content = None
-def func(name, number):
-   dic.update({name : number})
-   f = open("Contacts.txt", "a")
-
-   #dup remove+++++++++++++++++++++++++++
-   with open("Contacts.txt", "r") as w:
-      file = w.readlines()
-      for line in file:
-          wordlist = []
-          badlist = []
-          wordlist.append(line)
-          q=set()
-          q.add(line)
-      if q == wordlist:
-          f.write(name + ": " + number + "\n")
-          print("the contacts have been updated")
-      else:
-         for line in file:
-            if line in wordlist:
-               badlist.append(line)
-            else:
-               wordlist.append(line)
-         print("it's already in the file")
-
-
-   file = open("Contacts.txt", 'w')
-   for line in wordlist:
-      file.write(line)
-
-
-
-
+Contacts = {}
 
 while True:
-   name = input("enter the name:  \n")
-   number = input("enter the number: \n")
-   func(name, number)
-   x = input("do you want to add more?: Y/N \n").lower()
-   if x != "y":
-       break
+   global x
+   x = 0
+   x = int(input("1_Add new contact\n2_Remove a contact\n3_Edit number\n4_View contacts\n5_Exit\n"))
 
+   #ADD NEW
+   if x == 1:
+      Contacts.update({input("please enter the name: \n"):int(input("enter the number: \n"))})
+      print(Contacts)
+
+   # REMOVE
+   elif x == 2:
+      Contacts.pop(input("enter the name you want to be removed"))
+
+   # EDIT
+   elif x == 3:
+      try:
+
+         Contacts[input("Enter the contact's name whos number you want to edit: \n")] = int(input("Enter a number: \n"))
+      except:
+         print("Contact name does not exist")
+
+   # VIEW
+   elif x == 4:
+      print("----------")
+      for i in Contacts:
+         print(i)
+         print(Contacts[i])
+         print("----------")
+
+
+      '''
+   # EXIT (else it if no more options dont forget)
+   elif
+
+   # I DONT KNOW MAN
+   else
+'''
+   if input("Continue?: Y/N").lower() != y:
+      break
