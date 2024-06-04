3456a257018800b44989397b1b06cc8e9df6f4be
shahryarbmd <53258500+shahryarbmd@users.noreply.github.com>
2024-06-04 10:32:09 +0330

    e

---
+++ Hangman.py
@@ -0,0 +1,18 @@
+
+def fashion():
+    fash = {
+        "Baggy jeans": "_____ _____",
+        "Thigh highs": "_____ _____",
+        "Baseball cap": "________ ___",
+
+    }
+
+
+def food():
+    foo = {
+        "Hamburger": "_________",
+        "Waffles": "_______",
+        "Cheese cake": "______ ____"
+
+    }
+
