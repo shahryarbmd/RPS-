cd5fc34919a86ae4dd0f1512875f1b51eacaa056
shahryarbmd <53258500+shahryarbmd@users.noreply.github.com>
2024-07-23 16:49:46 +0330

    Update Guessing number.py

--- Guessing number.py
+++ Guessing number.py
@@ -1,15 +1,5 @@
-import random,time,os
-'''
-need one empty slot for the number to switch rapidly and set to a random one between 1 and 20
-then players guesses over and under while he sees the list of numbers plus the one chosen randomly
-then
-roll another number to decide if it is correct
-if correct add a point if not remove the points
+import time
 
-how to clear outputted messages
-with /r
-
-'''
 point = 0
 
 def over(num):
@@ -18,17 +8,15 @@ def over(num):
 def under(num):
    pass
 
+
+
 while True:
-   if input("are you ready to start?: Y/N \n").lower() != "y":
-       break
-   else:
-       print("good luck!")
-
-   for i in range(1,21):
-
-      print(i, end='\r')
-      time.sleep(0.5)
-
-
+    if input("are you ready to start?: Y/N \n").lower() != "y":
+        break
+    else:
+        print("good luck!")
+        for i in range(1,21):
+            print(i, end='\r')
+            time.sleep(0.5)
 
 
